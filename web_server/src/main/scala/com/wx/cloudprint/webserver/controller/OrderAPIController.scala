895762f04515abfbd205dad25918df9188a0fc25
wxpker <120443910@qq.com>
2017-09-28 17:28:01 +0800

    order

--- web_server/src/main/scala/com/wx/cloudprint/webserver/controller/OrderAPIController.scala
+++ web_server/src/main/scala/com/wx/cloudprint/webserver/controller/OrderAPIController.scala
@@ -1,18 +1,15 @@
 package com.wx.cloudprint.webserver.controller
 
-import java.util
-import java.util.stream.Collectors
-import javax.servlet.http.{HttpServletRequest, HttpSession}
+
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import com.google.gson.Gson
-import com.wx.cloudprint.dataservice.entity.Point
+import com.wx.cloudprint.dataservice.entity.{Dispatch, Point}
 import com.wx.cloudprint.dataservice.service.{AddressService, PointService}
-import com.wx.cloudprint.message.Message
-import com.wx.cloudprint.util.JsonUtil
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, ResponseBody, RestController}
-import scala.collection.JavaConverters._
+
 
 @RestController
 @RequestMapping(value = Array("/API"))
@@ -33,42 +30,53 @@ class OrderAPIController {
   @RequestMapping(value = Array("/order/verify"), method = Array(RequestMethod.GET, RequestMethod.POST))
   @ResponseBody
   def verify(param: String) = {
+    import OrderAPIController.caculatePrice
     val session = request.getSession
     val json = parse(param)
     val pointId = json \ "id"
-    val files = json \ "files"
     val point = pointService.get(pointId.extract[String])
-    val priceMap = scala.util.parsing.json.JSON.parseFull(point.getPrice).get.asInstanceOf[List[Map[String, Any]]]
-    var sum=0f
-    val price =  for (JObject(child) <- files) {
+
+    val sum = caculatePrice(param, point.getPrice, point.getDispatch)
+
+
+
+  }
+}
+
+object OrderAPIController extends App {
+
+  def caculatePrice(order: String, price: String, dispatch: Dispatch) = {
+    implicit val formats = DefaultFormats
+    val json = parse(order)
+    val files = json \ "files"
+    val priceMap = parse(price).extract[List[Map[String, Any]]]
+    var sum = 0f
+    for (JObject(child) <- files) {
       val values = child.toMap
       val copies = values("copies").extract[Int]
       val caliper = values("caliper").extract[String]
       val color = values("color").extract[String]
       val side = values("side").extract[Int]
       val size = values("size").extract[String]
-      var page = values("endPage").extract[Int] - values("startPage").extract[Int]
+      var page = values("endPage").extract[Int] - values("startPage").extract[Int] + 1
       val map = priceMap.filter(x => {
-        x("caliper") == caliper && x("size") == side
-      }).head("money").asInstanceOf[Map[String,Map[String,Any]]]
-     val perPrice= if(side==1)  map("oneside").toString().toFloat else map("duplex").toString().toFloat
-      page=if(page>0) page else 0
-      sum += page*perPrice
+        x("caliper").equals(caliper) && x("size").equals(size)
+      }).head("money").asInstanceOf[Map[String, Map[String, Any]]]
+      val money = map(color)
+      val perPrice = if (side == 1) money("oneside") else money("duplex")
+      page = if (page > 0) page else 0
+      sum += page * perPrice.toString.toFloat * copies
     }
+    sum + (if(dispatch==null) 0f
+    else dispatch.getDistributionCharge)
 
 
-}
+  }
 
-object OrderAPIController extends App {
-
-  implicit val formats = DefaultFormats
 
   val str = "[{\"caliper\":\"70g\",\"size\":\"A4\",\"money\":{\"mono\":{\"oneside\":1.0,\"duplex\":2.0},\"colorful\":{\"oneside\":1.0,\"duplex\":2.0}}}]"
-  val priceMap = scala.util.parsing.json.JSON.parseFull(str).get.asInstanceOf[List[Map[String, Any]]]
-  priceMap.filter(x => {
-    x("caliper") == "" && x("size") == ""
-  })
-  println(priceMap)
+  val order = "{\n  \"id\": \"0026\",\n  \"files\": [{\n    \"fileID\": \"A52B4686E173B0612B71148F7F9E099A\",\n    \"fileName\": \"申报指南.docx\",\n    \"layout\": 2,\n    \"copies\": 1,\n    \"size\": \"A4\",\n    \"caliper\": \"70g\",\n    \"color\": \"mono\",\n    \"side\": 1,\n    \"startPage\": 0,\n    \"endPage\": 0\n  }],\n  \"money\": 25,\n  \"couponID\": 0,\n  \"reduction\": {\n    \"newUser\": false,\n    \"full\": []\n  },\n  \"dispatching\": {\n    \"username\": \"\",\n    \"userPhone\": \"\",\n    \"address\": \"\",\n    \"leftMessage\": \"\"\n  }\n}"
 
+ println(caculatePrice(order,str,null))
 
 }
--- web_server/src/main/scala/com/wx/cloudprint/webserver/service/SignService.scala
+++ web_server/src/main/scala/com/wx/cloudprint/webserver/service/SignService.scala
@@ -2,11 +2,13 @@ package com.wx.cloudprint.webserver.service
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 import scalaj.http.Http
-import scala.util.parsing.json.JSON
 @Service
 class SignService {
+  implicit val formats = DefaultFormats
+
   @Value("${sign.signin.url}")
   var signinUrl:String=_
   @Value("${sign.register.url}")
@@ -15,20 +17,20 @@ class SignService {
   var getCodeUrl:String=_
   def signin(userName:String,password:String)={
     val body=Http(signinUrl).postForm(Seq("userName" -> userName, "password" -> password)).asString.body
-    val map=JSON.parseFull(body).get.asInstanceOf[Map[String,Map[String,String]]]
+    val map=parse(body).extract[Map[String,Map[String,String]]]
     map
 
 
   }
   def getCode(tel:String)={
          val body=Http(getCodeUrl).postForm(Seq("userName" -> tel)).asString.body
-    val map=JSON.parseFull(body).get.asInstanceOf[Map[String,Map[String,String]]]
+    val map=parse(body).extract[Map[String,Map[String,String]]]
     map
   }
   def register(userName:String,password:String,code:String)={
     val body=Http(registerUrl).postForm(Seq("userName" -> userName, "password" -> password,"code"->code)).asString.body
 
-    val map=JSON.parseFull(body).get.asInstanceOf[Map[String,Map[String,String]]]
+    val map=parse(body).extract[Map[String,Map[String,String]]]
     map
 
   }

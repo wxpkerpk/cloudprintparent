101f34e313d8138d48758d0b1fbc792713471652
wxpkerpk <120443910@ww.com>
2017-10-10 23:56:32 +0800

    link server to server

--- data_service/src/main/java/com/wx/cloudprint/dataservice/service/OrderService.java
+++ data_service/src/main/java/com/wx/cloudprint/dataservice/service/OrderService.java
@@ -5,6 +5,7 @@ import com.wx.cloudprint.dataservice.dao.UserDao;
 import com.wx.cloudprint.dataservice.entity.Order;
 import com.wx.cloudprint.dataservice.entity.User;
 import com.wx.cloudprint.dataservice.utils.EntityNameUtil;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,15 +39,35 @@ public class OrderService {
         return orderDao.findOne(id);
     }
     public List<Order> getOrders(Integer page,Integer rows,String type){
-        String sql=String.format("from "+ EntityNameUtil.getEntityName(Order.class) +" u where u.payState='%s'",type);
+        Session session= sessionFactory.openSession();
+        String sql;
+        if("ALL".equals(type)){
+            sql=String.format("from "+ EntityNameUtil.getEntityName(Order.class) +"",type);
 
-        return (List<Order>) sessionFactory.getCurrentSession().createQuery(sql).setFirstResult((page-1)*rows).setMaxResults(rows).list();
+        }else {
+            sql = String.format("from " + EntityNameUtil.getEntityName(Order.class) + " u where u.payState='%s'", type);
+        }
+        List<Order>orderList= (List<Order>) session.createQuery(sql).setFirstResult((page-1)*rows).setMaxResults(rows).list();
+        session.close();
+        return orderList;
     }
     public Map<String,Integer>getStatics(String userId){
 
+       Session session= sessionFactory.openSession();
         String sql=String.format("SELECT count(*) as numbers,user_id ,pay_state FROM orders WHERE user_id='%s' GROUP BY pay_state",userId);
-        sessionFactory.getCurrentSession().createSQLQuery(sql).list();
-        return null;
+      List result=  session.createSQLQuery(sql).list();
+      Map<String,Integer>resultMap=new HashMap<>();
+        session.close();
+        result.forEach(x->{
+            Object[]values= (Object[])x;
+
+            String key=values[2]==null?"":values[2].toString();
+            Integer value=Integer.parseInt(values[0].toString());
+            resultMap.put(key,value);
+
+
+        });
+        return resultMap;
 
 
     }

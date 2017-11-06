f8a74d94d463781e4e25c9de2a0d209745c30495
wxpkerpk <120443910@ww.com>
2017-11-06 23:46:01 +0800

    link server to server

--- background/src/main/java/com/geekcattle/StartBackgroudApplication.java
+++ background/src/main/java/com/geekcattle/StartBackgroudApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc//启动MVC
 @EnableTransactionManagement // 启注解事务管理
 @SpringBootApplication//SpringBoot启动核心
-public class Application extends WebMvcConfigurerAdapter  {
+public class StartBackgroudApplication extends WebMvcConfigurerAdapter {
 
     /**
      * 如果要发布到自己的Tomcat中的时候，需要继承SpringBootServletInitializer类，并且增加如下的configure方法。
@@ -25,7 +25,7 @@ public class Application extends WebMvcConfigurerAdapter  {
     }*/
 
     public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(StartBackgroudApplication.class, args);
     }
 
 }

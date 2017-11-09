41c11c7f3989771c34b378a97763d179d07d1d88
wx <wxpker@gmail.com>
2017-11-09 17:33:42 +0800

    1

--- background/src/main/scala/com/wx/cloudprint/controller/OrdersController.scala
+++ background/src/main/scala/com/wx/cloudprint/controller/OrdersController.scala
@@ -1,5 +1,9 @@
 package com.wx.cloudprint.controller
 
+import com.wx.cloudprint.dataservice.entity.Order
+import com.wx.cloudprint.dataservice.service.OrderService
+import com.wx.cloudprint.dataservice.utils.JqGridPageView
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation._
 
@@ -8,9 +12,24 @@ import org.springframework.web.bind.annotation._
 class OrdersController {
 
 
+  @Autowired
+  var orderservice:OrderService =_
+
   @RequestMapping(value = Array("/index"), method = Array(RequestMethod.GET))
   def index(): String = {
     "console/order/index"
+  }
+
+  @RequestMapping(value = Array("/search"), method = Array(RequestMethod.GET))
+  @ResponseBody
+  def search()={
+
+    val list=orderservice.search(null,null,null,null,null,"id","desc",1,10)
+    val jqGridPageView=new JqGridPageView[Order]()
+    jqGridPageView.setMaxResults(list.getTotalCount)
+    jqGridPageView.setRows(list.getResultList)
+    jqGridPageView
+
   }
 
 
